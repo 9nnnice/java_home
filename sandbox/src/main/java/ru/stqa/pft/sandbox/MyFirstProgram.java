63a1ec7ef74f02774c7d38be8cec7f2ded19a248
Kukushka <kostya.devyatkin92@mail.ru>
2021-06-01 21:48:47 +0300

    Задание №2 (п.1 - п.2)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,11 +12,22 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+// задание №2
+    Point c = new Point(1, 3);
+    Point d = new Point(2, 4);
+    System.out.println("Расстояние между двумя точками: " + Math.sqrt(distance(c.a, d.a) * distance(c.a, d.a) + distance(c.b, d.b) * distance(c.b, d.b)));
+
   }
 
   public static void hello(String somebody) { // объявление внутри переменной нужно для того, чтобы была возможность менять параметр извне. void - функция не возвращает никакое значение
 
     System.out.println("Hello," + somebody + "!");
+  }
+
+  // задание №2
+  public static double distance(double p1, double p2) {
+    return p1 - p2;
   }
 
 }
