f69ccc6c089c1fdc09fb0b68b074b388e07caeaf
Kukushka <kostya.devyatkin92@mail.ru>
2021-07-03 17:02:40 +0300

    Задание №5 (ч.4.2)
    Перенос вспомогательных методов в ApplicationManager (с помощью рефакторинга в два шага)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,9 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.concurrent.TimeUnit;
 
@@ -32,5 +34,32 @@ public class ApplicationManager {
 
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
+  }
+
+  public void SubmitContactCreation() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  public void fillContactForm(ContactData groupDataForm) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(groupDataForm.getName1());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("middlename")).clear();
+    wd.findElement(By.name("middlename")).sendKeys(groupDataForm.getName2());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(groupDataForm.getNumber());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(groupDataForm.getMail());
+  }
+
+  public void initContactCreation() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  public void returnToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,19 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    SubmitContactCreation();
-    fillContactForm(new ContactData("Тест", "Тестович", "+79098887766", "123@mail.ru"));
-    initContactCreation();
-    returnToHomePage();
+    app.SubmitContactCreation();
+    app.fillContactForm(new ContactData("Тест", "Тестович", "+79098887766", "123@mail.ru"));
+    app.initContactCreation();
+    app.returnToHomePage();
   }
 
 }
